e5ea2a3a0bba742cdcb1da2cd5312ea41632524e
zelalem-fisseha <zelalem.fisseha@bitscollege.edu.et>
2025-02-10 17:45:52 +0300

    initial commit

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Block"},{"p":"<Unnamed>","l":"Blockchain"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
---
+++ src/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * A simple blockchain implementation demonstrating core blockchain concepts.
+ * 
+ * This package contains classes that implement a basic blockchain with features including:
+ * <ul>
+ *   <li>Block creation and management</li>
+ *   <li>Blockchain validation</li>
+ *   <li>Hash generation and verification</li>
+ *   <li>Chain integrity checking</li>
+ * </ul>
+ * 
+ * The implementation is intended for educational purposes and demonstrates:
+ * <ul>
+ *   <li>Basic blockchain structure and operations</li>
+ *   <li>Hash-based block linking</li>
+ *   <li>Simple integrity validation</li>
+ * </ul>
+ * 
+ * @version 1.0
+ * @since 2024-01-01
+ */
+package blockchain; 
